523419a2660c8e237367bea6827412841d27f2ef
Jayana10 <jayana10302@gmail.com>
2025-02-11 10:53:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,31 +13,13 @@ class Car {
 class SportsCar extends Car {
     constructor(make, model, topSpeed) {
         super(make, model); 
-        this.topSpeed = topSpeed;
     }
 
     getTopSpeed() {
         return this.topSpeed;
     }
 }
-SportsCar.prototype=object.create(Car.prototype);
 
 // Do not change this
 window.Car = Car;
 window.SportsCar = SportsCar;
-// class Car{
-// 	constructor(make,model){
-// 		this.make = make;
-// 		this.model=model;
-// 	}
-// }
-// class SportsCar extends Car{
-// 	constructor(make,model,topSpeed){
-// 		super(make,model);
-// 		this.topSpeed=topSpeed;
-// 	}
-// }
-
-// Do not change the code below
-// window.Car = Car;
-// window.SportsCar = SportsCar;
