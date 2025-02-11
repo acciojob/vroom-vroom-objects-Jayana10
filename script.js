717d36b2c8c7a76f68e746093e5446faaa047dc6
Jayana10 <jayana10302@gmail.com>
2025-02-11 11:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
 // Car Constructor Function
-function Car(make, model) {
-    this.make = make;
-    this.model = model;
-}
+// function Car(make, model) {
+//     this.make = make;
+//     this.model = model;
+// }
 
-// Add method to Car prototype
-Car.prototype.getMakeModel = function () {
-    return `${this.make} ${this.model}`;
-};
+// // Add method to Car prototype
+// Car.prototype.getMakeModel = function () {
+//     return `${this.make} ${this.model}`;
+// };
 
-// SportsCar Constructor Function (Inheriting from Car)
-function SportsCar(make, model, topSpeed) {
-    // Call the Car constructor
-    Car.call(this, make, model);
+// // SportsCar Constructor Function (Inheriting from Car)
+// function SportsCar(make, model, topSpeed) {
+//     // Call the Car constructor
+//     Car.call(this, make, model);
     
-    // Add SportsCar-specific property
-    this.topSpeed = topSpeed;
-}
+    
+//     this.topSpeed = topSpeed;
+// }
 
-// Inherit from Car prototype
-SportsCar.prototype = Object.create(Car.prototype);
 
-// Restore constructor reference
-SportsCar.prototype.constructor = SportsCar;
+// SportsCar.prototype = Object.create(Car.prototype);
 
-// Add getTopSpeed method to SportsCar prototype
-SportsCar.prototype.getTopSpeed = function () {
-    return this.topSpeed;
-};
+
+// SportsCar.prototype.constructor = SportsCar;
+
+
+// SportsCar.prototype.getTopSpeed = function () {
+//     return this.topSpeed;
+// };
 
 // Do not change this
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    getMakeModel() {
+        return `${this.make} ${this.model}`; // ✅ Corrected template literal
+    }
+}
+
+class SportsCar extends Car {
+    constructor(make, model, topSpeed) {
+        super(make, model); // ✅ Calls the parent constructor
+        this.topSpeed = topSpeed;
+    }
+
+    getTopSpeed() {
+        return this.topSpeed;
+    }
+}
+
+// Do not change this
+
+
+
 window.Car = Car;
 window.SportsCar = SportsCar;
